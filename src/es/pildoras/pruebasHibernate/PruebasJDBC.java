43fca10ce71856526fa7c43c871d15136767be60
nahueld97 <nahueld97@outlook.com>
2023-01-16 23:03:29 -0300

    Initial commit with hibernate configuration

---
+++ src/es/pildoras/pruebasHibernate/PruebasJDBC.java
@@ -0,0 +1,22 @@
+package es.pildoras.pruebasHibernate;
+
+import java.sql.*;
+
+public class PruebasJDBC {
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/puebashibernate?useSSL=false";
+		String user="root";
+		String pass="bd-dcic";
+		try {
+			System.out.println("Intentando conectar con la BBDD: " + jdbcUrl);
+			Connection connection= DriverManager.getConnection(jdbcUrl, user, pass);
+			System.out.println("Conexion exitosa!");
+			
+			
+			
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
